4085ac58181e60085ec6c9c63b58be6802eb9622
ILOveMyasko <brutalkin_v@mail.ru>
2025-03-16 15:11:14 +0300

    Initial commit

---
+++ src/main/java/main/lab1/Lab1Application.java
@@ -0,0 +1,15 @@
+package main.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+	public static void main(String[] args) {
+		System.out.println("da");
+		//SpringApplication.run(Lab1Application.class, args);
+
+	}
+
+}
