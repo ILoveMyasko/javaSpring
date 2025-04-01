49a3122430a3c2a2462a609a7415c87046c22e7b
ILOveMyasko <brutalkin_v@mail.ru>
2025-04-01 19:31:51 +0300

    Fixed UserServiceTest by mocking database responses

--- src/test/java/main/lab1/serviceTests/UserServiceTest.java
+++ src/test/java/main/lab1/serviceTests/UserServiceTest.java
@@ -3,6 +3,7 @@ package main.lab1.serviceTests;
 import main.lab1.model.User;
 import main.lab1.exceptions.UserAlreadyExistsException;
 import main.lab1.exceptions.UserNotFoundException;
+import main.lab1.repos.UserRepository;
 import main.lab1.services.UserService;
 import main.lab1.services.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,69 +13,101 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 //unit tests
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
     private UserServiceImpl userService;
 
-    @BeforeEach
-    void setUp() {
-        userService = new UserServiceImpl();
-    }
+//    @BeforeEach
+//    void setUp() {
+//        userService = new UserServiceImpl();
+//    }
 
     @Test
     void createUser_WithNewId_ShouldAddUser() {
         long userId = 1;
         User user = new User(userId, "Alex", "alex@ex.com");
-        long usersCount = userService.getAllUsers().size();
+
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+        when(userRepository.save(user)).thenReturn(user);
+        when(userRepository.findAll())
+                .thenReturn(List.of()); // After creation
+        long initialUserCount = userRepository.count();
         userService.createUser(user);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(userRepository.findAll())
+                .thenReturn(List.of(user));
+
+
         assertDoesNotThrow(() -> userService.getUserById(userId));
         assertEquals(user, userService.getUserById(userId));
-        assertEquals(usersCount+1, userService.getAllUsers().size());
+        assertEquals(initialUserCount+1, userService.getAllUsers().size());
     }
 
     @Test
     void createUser_WithExistingId_ShouldThrowException() {
-        long userId = 1;
-        User user = new User(userId, "Alex", "alex@ex.com");
-        User user2 = new User(userId, "Anna", "anna@example.com");
-        userService.createUser(user);
-        assertThrows(UserAlreadyExistsException.class,
-                () -> userService.createUser(user2)
-        );
+        long existingUserId = 1;
+        User newUser = new User(existingUserId, "NewUser", "new@ex.com");
+
+        when(userRepository.existsById(existingUserId)).thenReturn(true);
+        assertThrows(UserAlreadyExistsException.class, () -> {
+            userService.createUser(newUser);
+        });
+
+        verify(userRepository, never()).save(any());
+        verify(userRepository).existsById(existingUserId); // was called
     }
 
     @Test
     void getUser_WithExistingId_ShouldReturnUser() {
-        User expectedUser = new User(1, "Alex", "alex@ex.com");
+        long existingUserId = 1;
+        User expectedUser = new User(existingUserId, "Alex", "alex@ex.com");
+        when(userRepository.findById(existingUserId)).thenReturn( Optional.of(expectedUser));
         userService.createUser(expectedUser);
-        User actualUser = userService.getUserById(1);
-        assertEquals(expectedUser, actualUser);//ignore that we can throw an exception because this fails the test anyway
+        User actualUser = userService.getUserById(existingUserId);
+        assertEquals(expectedUser, actualUser);
     }
 
     @Test
     void getUser_WithNonExistentId_ShouldThrowException() {
-        User user = new User(1, "Alex", "alex@ex.com");
-        userService.createUser(user);
+        long newUserId = 1;
+        //User user = new User(newUserId, "Alex", "alex@ex.com");
+        when(userRepository.findById(newUserId)).thenReturn(Optional.empty());
         assertThrows(UserNotFoundException.class, //what exception will be thrown
-                () -> userService.getUserById(2));
+                () -> userService.getUserById(newUserId));
     }
 
     @Test
     void getAllUsers_WithNoUsers_ShouldReturnEmptyList() {
+        when(userRepository.findAll()).thenReturn(List.of());
         assertTrue(userService.getAllUsers().isEmpty());
     }
 
     @Test
     void getAllUsers_WithUsers_ShouldReturnListOfAllUsers() {
-        User user1 = new User(1, "Alex", "alex@example.com");
-        User user2 = new User(2, "Anna", "anna@example.com");
+        long userId = 1;
+        User user1 = new User(userId, "Alex", "alex@example.com");
+        User user2 = new User(userId+1, "Anna", "anna@example.com");
+
+        when(userRepository.existsById(userId)).thenReturn(false);
+        when(userRepository.existsById(userId+1)).thenReturn(false);
         userService.createUser(user1);
         userService.createUser(user2);
+        when(userRepository.findAll()).thenReturn(List.of(user1,user2));
         List<User> users = userService.getAllUsers();
         assertAll(//assert there are exactly 2 users and both inserted are present
                 () -> assertEquals(2, users.size()),
