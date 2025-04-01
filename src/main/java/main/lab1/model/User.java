b27cf4e4ef15744a1cab0c8d38afc71f3f0b77ef
ILOveMyasko <brutalkin_v@mail.ru>
2025-04-01 23:45:59 +0300

    docker fixed, running fine

--- src/main/java/main/lab1/model/User.java
+++ src/main/java/main/lab1/model/User.java
@@ -4,8 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-@Data
-@EqualsAndHashCode
+@Data //not recommended?
 @AllArgsConstructor
 @NoArgsConstructor //mandatory for SpringJPA
 @Entity
