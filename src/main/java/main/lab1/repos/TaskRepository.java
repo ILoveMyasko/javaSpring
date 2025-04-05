65a0ae0ca2f7819e64682cd43e1dbfc93d616174
ILOveMyasko <brutalkin_v@mail.ru>
2025-04-05 16:20:45 +0300

    comments added

--- src/main/java/main/lab1/repos/TaskRepository.java
+++ src/main/java/main/lab1/repos/TaskRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
-    List<Task> findByUserId(Long userId);
+    List<Task> findByUserId(Long userId); //find all would've been better but this works too
 }
