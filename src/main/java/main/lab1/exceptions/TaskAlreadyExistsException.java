d509855599f2ed521d89944ec3d0af77248342e5
ILOveMyasko <brutalkin_v@mail.ru>
2025-05-10 13:26:23 +0300

    combined exceptions

--- src/main/java/main/lab1/exceptions/TaskAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package main.lab1.exceptions;
-
-public class TaskAlreadyExistsException extends RuntimeException {
-  public TaskAlreadyExistsException(int id) {
-    super("Task with id = " + id + " already exists.");
-  }
-}
