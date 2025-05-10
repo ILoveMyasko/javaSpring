446b45b662147439ef67d76f3e8d1304ed54abe2
ILOveMyasko <brutalkin_v@mail.ru>
2025-05-10 13:28:12 +0300

    added email duplicate check

--- src/main/java/main/lab1/services/UserServiceImpl.java
+++ src/main/java/main/lab1/services/UserServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import main.lab1.entities.User;
-import main.lab1.exceptions.UserAlreadyExistsException;
+import main.lab1.exceptions.DuplicateResourceException;
 import main.lab1.exceptions.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +16,19 @@ public class UserServiceImpl implements UserService {
 
     private final Map<Integer, User> users = new HashMap<Integer, User>(); // can skip <> params since java 7
 
-    public void createUser(User user) {
-        if (users.containsKey(user.getId())) {
-            throw new UserAlreadyExistsException(user.getId());
+    public User createUser(User newUser) {
+        if (users.containsKey(newUser.getUserId())) {
+            throw new DuplicateResourceException("User with id " + newUser.getUserId() + " already exists");
         }
-        users.put(user.getId(), user);
+        if (users.values()
+                 .stream()
+                 .anyMatch(user -> user.getEmail().equalsIgnoreCase(newUser.getEmail())))
+        {
+            throw new DuplicateResourceException("User with email " + newUser.getEmail() + " already exists");
+        }
+
+        users.put(newUser.getUserId(), newUser);
+        return newUser;
     }
 
     public User getUserById(int id) {
