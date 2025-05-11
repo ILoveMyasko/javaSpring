d57413e067e3e84c6edb370351cfcbf85a521142
ILOveMyasko <brutalkin_v@mail.ru>
2025-05-11 14:05:05 +0300

    combined flyway init scripts in one

--- src/main/resources/db/migration/V1__initusers.sql
+++ src/main/resources/db/migration/V1__initusers.sql
@@ -1,8 +1,28 @@
-
 CREATE TABLE users
 (
     user_id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
     name    VARCHAR(50) NOT NULL,
-    email   VARCHAR(50),
+    email   VARCHAR(50) NOT NULL,
     CONSTRAINT pk_users PRIMARY KEY (user_id)
 );
+
+CREATE TABLE tasks (
+                       task_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                       user_id BIGINT NOT NULL ,
+                       task_title VARCHAR(100) NOT NULL,
+                       task_description VARCHAR(255),
+                       created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+                       expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+                       is_completed BOOLEAN NOT NULL
+);
+
+CREATE TABLE notifications
+(
+    notification_id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    user_id         BIGINT NOT NULL,
+    task_id         BIGINT NOT NULL,
+    text            VARCHAR(255),
+    created_at      TIMESTAMP WITH TIME ZONE,
+    CONSTRAINT pk_notifications PRIMARY KEY (notification_id)
+);
+
