b01413f8f9c81880e30025831e6c871cd8f0908c
ILOveMyasko <brutalkin_v@mail.ru>
2025-05-13 12:44:58 +0300

    removed unused imports and comments

--- src/main/java/main/lab1/Lab1Application.java
+++ src/main/java/main/lab1/Lab1Application.java
@@ -1,12 +1,8 @@
 package main.lab1;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 //Springboot version 3.4.3
@@ -14,8 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableAsync
 @EnableScheduling
-//@EnableTransactionManagement
-//@EnableKafka //can be omitted with my default kafka setup
+
 public class Lab1Application {
 
 	public static void main(String[] args) {
